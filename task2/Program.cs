348b403e8534fa18b29d9fa6d6591c23b0039ebf
Наталья Ярцева <zabaluna@mail.ru>
2022-10-19 00:01:04 +0300

    Three numbers for two numbers

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+int x1 = number/100;
+int x2 = number%10;
+
+Console.WriteLine($"{x1}{x2}");
