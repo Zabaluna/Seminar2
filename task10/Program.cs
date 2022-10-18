914f1e7f1651831bbb59eeeb5df1358ff79536bc
Наталья Ярцева <zabaluna@mail.ru>
2022-10-19 00:41:55 +0300

    Second number of three print

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+
+// int x1 = number/100;
+// Console.WriteLine($"{x1}");
+int x2 = number/10;
+// Console.WriteLine($"{x2}");
+
+int x3 = x2 % 10;
+Console.WriteLine($"{x3}");
+
