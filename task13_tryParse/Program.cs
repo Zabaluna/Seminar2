cb1c438d5ce1398ba5ed9d242fc5f7d336b6cfee
Наталья Ярцева <zabaluna@mail.ru>
2022-10-20 00:01:19 +0300

    Third number no

---
+++ task13_tryParse/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+
+if(int.TryParse(number, out int num))
+{
+   string N = Convert.ToString(Math.Abs(num));
+   if(Math.Abs(num)>99)
+   {
+    Console.WriteLine($"Третья цифра введенного числа: {N[2]} ");
+   }
+   else
+   Console.WriteLine("Третьей цифры нет в числе");
+}
+else Console.WriteLine("Вы ввели не число");
