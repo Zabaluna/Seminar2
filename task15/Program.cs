a2becdce84a44086021223e773df60c6ed4a544c
Наталья Ярцева <zabaluna@mail.ru>
2022-10-20 00:06:51 +0300

    Weekand days long

--- task15/Program.cs
+++ task15/Program.cs
@@ -3,52 +3,46 @@
 Console.WriteLine("Введите число от 1 до 7: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
-// if( n > 0 && n <= 7)
-// {
-//     if (n == 1)
-//     {
-//      Console.Write("Рабочий день ");
-//     }
+if( n > 0 && n <= 7)
+{
+    if (n == 1)
+    {
+     Console.Write("Рабочий день ");
+    }
 
-//     if (n == 2) 
-//     {
-//     Console.Write("Рабочий день: ");
-//     }
+    if (n == 2) 
+    {
+    Console.Write("Рабочий день: ");
+    }
 
-//     if (n == 3) 
-//     {
-//     Console.Write("Рабочий день: ");
-//     }
+    if (n == 3) 
+    {
+    Console.Write("Рабочий день: ");
+    }
 
-//     if (n == 4)
-//     {
-//      Console.Write("Рабочий день: ");
-//     }
+    if (n == 4)
+    {
+     Console.Write("Рабочий день: ");
+    }
 
-//     if (n == 5) 
-//     {
-//     Console.Write("Рабочий день  ");
-//     }
+    if (n == 5) 
+    {
+    Console.Write("Рабочий день  ");
+    }
 
-//     if (n == 6) 
-//     {
-//     Console.Write("Ура!!! Выходной:-) ");
-//     }
+    if (n == 6) 
+    {
+    Console.Write("Ура!!! Выходной:-) ");
+    }
 
-//     if (n == 7)
-//     {
-//      Console.Write("Ура!!! Выходной:-)" );
-//     }
-// }
-// else 
-//    {
-//     Console.WriteLine("Попробуйте еще раз, введите число от 1 до 7: ");
-//    }
-
- if( n > 0 && n <= 7)
-{   
-    if (n == 6 | n == 7) Console.WriteLine("Ура!!! Выходной:-) ");
-    else Console.WriteLine("Рабочий день");
+    if (n == 7)
+    {
+     Console.Write("Ура!!! Выходной:-)" );
+    }
 }
-    else Console.WriteLine("Попробуйте еще раз, введите число от 1 до 7: ");
-   
+else 
+   {
+    Console.WriteLine("Попробуйте еще раз, введите число от 1 до 7: ");
+   }
+
+ 
