4b501ca635897b2e71472aaba7eefb4a5699bfd3
Наталья Ярцева <zabaluna@mail.ru>
2022-10-20 01:05:18 +0300

    Second number

---
+++ task10_tryParse/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+string? number = Console.ReadLine();
+
+if(int.TryParse(number, out int num))
+{
+   string N = Convert.ToString(Math.Abs(num));
+   if(Math.Abs(num) > 99 && Math.Abs(num) < 1000)
+   {
+    Console.WriteLine($"Третья цифра введенного числа: {N[2]} ");
+   }
+   else Console.Write("Третьей цифры нет в числе");
+}
+else Console.WriteLine("Вы ввели не число");
